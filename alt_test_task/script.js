9e19d1de82f31865d17251a79c32051a2dbd6e4e
gepolov <gepolov@gmail.com>
2020-03-12 16:38:46 +0300

    Update script.js

--- alt_test_task/script.js
+++ alt_test_task/script.js
@@ -4,10 +4,30 @@ var input  = document.querySelector("input");
 
 button.addEventListener("click", function() {
     if(input.value != '') {
-        var li = document.createElement('li');
-        li.innerText= input.value ;
-        list.appendChild(li);
-        input.value = '';
+        if ( /[^a-zA-Z]/.test(input.value)) {
+            var li = document.createElement('li');
+            li.innerText= input.value ;
+            var btnx = document.createElement('button');
+            btnx.innerText = 'x';
+            list.appendChild(li);
+            li.appendChild(btnx);
+
+            btnx.addEventListener("click", function(){
+                delel = btnx.parentNode;
+                delel.parentNode.removeChild(delel);
+            })
+
+
+
+
+            input.value = '';
+        }else {
+            var li = document.createElement('li');
+            li.innerText= input.value ;
+            list.appendChild(li);
+            input.value = '';
+        }
+
 
     }
 });
